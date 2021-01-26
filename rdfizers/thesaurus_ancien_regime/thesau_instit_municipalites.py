1c0fc018974e5acff942fb761558b8146ba1c516
Rebecca Bristow <58423312+rbbristow@users.noreply.github.com>
2021-01-26 10:43:25 +0100

    Update script institutions_municipalites.py

--- rdfizers/thesaurus_ancien_regime/thesau_instit_municipalites.py
+++ rdfizers/thesaurus_ancien_regime/thesau_instit_municipalites.py
@@ -1,6 +1,7 @@
 import argparse
 from rdflib import Graph, Literal, Namespace, DCTERMS, RDF, RDFS, SKOS, URIRef, URIRef as u, Literal as l
 import re
+import json
 
 ################################################################################
 ### Ouverture du graphe et du fichier texte
@@ -14,7 +15,11 @@ args = parser.parse_args()
 input_graph = Graph()
 input_graph.load(args.inputrdf)
 
-output_txt = open(args.outputtxt, "w")
+outputtxt = open(args.outputtxt, "w")
+
+################################################################################
+### Fonctions
+################################################################################
 
 def ro(s, p):
 	try:
@@ -33,20 +38,59 @@ def ro_list(s, p):
 ################################################################################
 
 for opentheso_municipalite_uri, p, o in input_graph.triples((URIRef("http://opentheso3.mom.fr/opentheso3/?idc=municipalite&idt=173"), RDF.type, SKOS.Concept)):
-	municipalites = ro_list(opentheso_municipalite_uri, SKOS.narrower)
-	for municipalite in municipalites:
-		institutions = ro_list(municipalite, SKOS.narrower)
-		for institution in institutions:
-			entrees = (ro_list(institution, SKOS.prefLabel))
 
-			lst = []
-			for entree in entrees:
-				if "Corps de ville" in entree:
-					lst.append("Corps de ville")
-					pass
+	# On récupère l'URI de la municipalité
+	municipalites_uri = ro_list(opentheso_municipalite_uri, SKOS.narrower)
 
+	lst = {}
+
+	# On récupère les URIs des institutions/fonctions de chaque municipalité
+	for municipalite in municipalites_uri:
+		institutions_uri = ro_list(municipalite, SKOS.narrower)
+
+		# On récupère leur nom
+		for institution in institutions_uri:
+			institution_prefLabel = (ro_list(institution, SKOS.prefLabel))
+
+			# On transforme chaque valeur en clé de dictionnaire
+			for chaine_caract in institution_prefLabel:
+
+				# On crée la clé "Corps de ville"
+				cle = "Corps de ville"
+				lst.setdefault(cle, [])
+
+				if "Corps de ville" in chaine_caract:
+					# On ajoute les valeurs de chaque clé en supprimant le nom de la municipalité
+					narrower = (ro(institution, SKOS.narrower))
+					if narrower != None:
+						narrower_prefLabel = str((ro(narrower, SKOS.prefLabel)))
+						n = re.search(".*(?= de| d\'A)", narrower_prefLabel)
+						if n != None:
+							n_group = n.group()
+							if n_group not in lst[cle]:
+								lst[cle].append(n_group)
+								pass
+
+				# On ajoute les autres clés en supprimant le nom de la municipalité
 				else:
-					e = re.search(".*(?=de)", entree)
-					print(e)
+					e = re.search(".*(?= de | d\'A)", chaine_caract)
+					if e != None:
+						e_group = e.group()
+						if e_group not in lst:
+							lst.setdefault(e_group, [])
 
-			#print(lst)
+							# On ajoute leurs valeurs en supprimant le nom de la municipalité
+						narrower = (ro(institution, SKOS.narrower))
+						if narrower != None:
+							narrower_prefLabel = str((ro(narrower, SKOS.prefLabel)))
+							if n != None:
+								n = re.search(".*(?= de | d\'A|)", narrower_prefLabel)
+								n_group = n.group()
+								if n_group not in lst[e_group]:
+									lst[e_group].append(n_group)
+
+
+test = json.dumps(lst, indent = 4, ensure_ascii=False)
+outputtxt.write(test)
+print(test)
+
