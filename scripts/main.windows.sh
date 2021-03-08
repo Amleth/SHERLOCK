165c690e2473b21f02cdc7a1671a81e31364a614
Rebecca Bristow <58423312+rbbristow@users.noreply.github.com>
2021-03-08 14:24:16 +0100

    Creation script bash main.py

---
+++ scripts/main.windows.sh
@@ -0,0 +1,4 @@
+python ./rdfizers/mercure_galant/main.py \
+    --tei "./sources/mercure_galant_tei" \
+    --ttl "./out/mercure_corpus.ttl" \
+    --corpus_cache "./caches/mercure_galant/cache_corpus.yaml"
