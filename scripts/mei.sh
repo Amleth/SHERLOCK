2adda2128518d0dc68763dd7a819af4925c15cd9
Thomas Bottini <thomas.bottini@cnrs.fr>
2021-04-02 14:56:48 +0200

    MEI

--- scripts/mei.sh
+++ scripts/mei.sh
@@ -3,13 +3,30 @@ DIR=$SCRIPT_DIR/..
 
 # MEI
 
+rm -rf $DIR/out/mei
+rm -rf $DIR/out/files/mei
+
 mkdir -p $DIR/caches/mei
 mkdir -p $DIR/out/mei
 mkdir -p $DIR/out/polymir
+mkdir -p $DIR/out/files/mei
 
 for f in $(ls $DIR/sources/mei/**/*.mei)
 do
     SHA1=$(shasum $f | awk '{print $1}')
+
+    cp $f $DIR/out/files/mei/$SHA1.mei
+    
+    PDF=$(dirname $f)/$(basename $f .mei).pdf
+    if test -f "$PDF"; then
+        cp $PDF $DIR/out/files/mei/$SHA1.pdf
+    fi
+    
+    SIB=$(dirname $f)/$(basename $f .mei).sib
+    if test -f "$SIB"; then
+        cp $SIB $DIR/out/files/mei/$SHA1.sib
+    fi
+    
     MEI_UUID=$(
         python3 $DIR/rdfizers/mei/main.py \
         --cache $DIR/caches/mei/$SHA1.yaml \
