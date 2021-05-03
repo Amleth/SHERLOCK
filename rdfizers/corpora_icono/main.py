ec775cfd294e2a49cca71503911fd211167106e1
Rebecca Bristow <58423312+rbbristow@users.noreply.github.com>
2021-05-03 15:16:33 +0200

    correction bug

--- rdfizers/corpora_icono/main.py
+++ rdfizers/corpora_icono/main.py
@@ -288,15 +288,26 @@ def traitement_images(sous_collection):
 			t(img_E36, a, crm("E36_Visual_Item"))
 			t(img_E36, crm("P1_has_identifier"), l(id))
 
-			## Lien avec la livraison
-			try:
-				livraison_F2 = she(
-					cache_corpus.get_uuid(["Corpus", "Livraisons", id_livraison, "Expression originale", "F2"]))
-				t(livraison_F2, crm("P148_has_component"), img_E36)
-			except:
-				pass
+			## Lien avec la livraison ou l'article
+			if img_row[3].value == None:
+				try:
+					livraison_F2 = she(
+						cache_corpus.get_uuid(["Corpus", "Livraisons", id_livraison, "Expression originale", "F2"]))
+					t(livraison_F2, crm("P148_has_component"), img_E36)
+				except:
+					pass
+			else:
+				id_article = img_row[3].value
+				if len(id_article) >= 15:
+					id_livraison = img_row[3].value[:-4]
+					article_F2 = she(cache_corpus.get_uuid(["Corpus", "Livraisons", id_livraison, "Expression originale", "Articles", id_article, "F2"]))
+					print(article_F2)
+				else:
+					id_livraison = img_row[3].value[:-3]
+					article_F2 = she(cache_corpus.get_uuid(
+						["Corpus", "Livraisons", id_livraison, "Expression originale", "Articles", id_article, "F2"]))
+					print(article_F2)
 
-			## TODO AJOUTER LIEN AVEC L'ARTICLE QUAND C'EST POSSIBLE
 
 			## Titre sur l'image (E13)
 			if img_row[7].value != None:
