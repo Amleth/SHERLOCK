56530cdf90b8b01577db44bd88b90514f5e143b6
Rebecca Bristow <58423312+rbbristow@users.noreply.github.com>
2021-05-12 15:46:10 +0200

    update mg_vocab_gravures.py

--- scripts/corpus_icono_MGE.sh
+++ scripts/corpus_icono_MGE.sh
@@ -1,9 +1,9 @@
-mkdir -p ./out/corpora_icono
-mkdir -p ./caches/corpora_icono
+mkdir -p ./out/corpora_icono/MGE
+mkdir -p ./caches/corpora_icono/MGE
 python3 ./rdfizers/corpora_icono/main.py \
  --collection_id "MGE" \
  --excel_coll "./sources/corpora_icono/MGE/MGE.xlsx" \
  --excel_index "./sources/corpora_icono/collections.xlsx" \
- --output_ttl "./out/corpora_icono/MGE.ttl" \
- --cache_40CM "./caches/corpora_icono/MGE.yaml" \
+ --output_ttl "./out/corpora_icono/MGE/MGE.ttl" \
+ --cache_40CM "./caches/corpora_icono/MGE/MGE.yaml" \
  --cache_corpus "./caches/mercure_galant/cache_corpus.yaml"
---
+++ scripts/mg_vocabulaire_gravures.sh
@@ -0,0 +1,4 @@
+python3 ./rdfizers/corpora_icono/mg_vocabulaire_gravures.py \
+    --xls "./sources/corpora_icono/MGE/vocabulaire_indexation.xslx" \
+    --ttl "./out/corpora_icono/MGE/vocabulaire_gravures.ttl" \
+    --cache "./caches/corpora_icono/MGE/vocabulaire_gravures.yaml"
