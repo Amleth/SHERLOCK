2171800c967f334d86a2de965d8807fbba25ec18
Rebecca Bristow <58423312+rbbristow@users.noreply.github.com>
2021-06-29 14:31:04 +0200

    udpate personnes_insert.py

--- directus/referentiel_ancien_regime/personnes_insert.py
+++ directus/referentiel_ancien_regime/personnes_insert.py
@@ -11,13 +11,14 @@ import sys
 import requests
 from pprint import pprint
 
+# Arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("--cache_personnes")
 parser.add_argument("--cache_corpus")
 parser.add_argument("--skos")
 args = parser.parse_args()
 
-# SECRET YAML
+# Secret YAML
 file = open(os.path.join(sys.path[0], "secret.yaml"))
 secret = yaml.full_load(file)
 r = requests.post(secret["url"] + "/auth/login", json={"email": secret["email"], "password": secret["password"]})
@@ -25,21 +26,17 @@ access_token = r.json()['data']['access_token']
 refresh_token = r.json()['data']['refresh_token']
 file.close()
 
-# LECTURE DES DONNEES DE LA BASE
-r = requests.get(secret["url"] + '/items/sources_articles/MG-1678-01_206?access_token=' + access_token)
+# Test - lecture de données
+# r = requests.get(secret["url"] + '/items/sources_articles/MG-1678-01_206?access_token=' + access_token)
 # pprint(r.json())
-r = requests.get(secret["url"] + '/items/sources_articles_indices/1?access_token=' + access_token)
+# r = requests.get(secret["url"] + '/items/sources_articles_indices/1?access_token=' + access_token)
 # pprint(r.json())
-r = requests.get(secret["url"] + '/items/sources_articles_indices/2?access_token=' + access_token)
+# r = requests.get(secret["url"] + '/items/sources_articles_indices/2?access_token=' + access_token)
 # pprint(r.json())
 
-# CACHES
+# Caches
 cache_corpus = Cache(args.cache_corpus)
 cache_personnes = Cache(args.cache_personnes)
-
-#####################################################################################################
-## RECUPERATION DES DONNEES DU FICHIER SKOS
-#####################################################################################################
 
 # Initialisation du graphe
 input_graph = Graph()
@@ -52,19 +49,31 @@ dict_infos_concept = {}
 dict_indexations = {}
 
 
+#########################################################################################
+## INSERTION DES PERSONNES ET LEURS INFORMATIONS DANS DIRECTUS
+#########################################################################################
+
+# RECUPERATION DES DONNEES
 for opentheso_personne_uri, p, o in input_graph.triples((None, RDF.type, SKOS.Concept)):
 
 	id = list(input_graph.objects(opentheso_personne_uri, DCTERMS.identifier))[0].value
+
+	# uuid
 	uuid = cache_personnes.get_uuid(["personnes", id, "uuid"])
 	dict_infos_concept["id"] = uuid
+
+	# prefLabel
 	label = list(input_graph.objects(opentheso_personne_uri, SKOS.prefLabel))[0].value
 	dict_infos_concept["label"] = label
+
+	# définition
 	definitions = list(input_graph.objects(opentheso_personne_uri, SKOS.definition))
 	if len(definitions) >= 1:
 		dict_infos_concept["definition"] = definitions[0].value
 	else:
 		dict_infos_concept["definition"] = None
 
+	# notes/indexation
 	notes = []
 	for p in [SKOS.editorialNote, SKOS.historyNote, SKOS.note, SKOS.scopeNote]:
 		notes_opentheso = list(input_graph.objects(opentheso_personne_uri, p))
@@ -77,6 +86,12 @@ for opentheso_personne_uri, p, o in input_graph.triples((None, RDF.type, SKOS.Co
 						continue
 					indexation = indexation.replace("##", "").strip().split(" ")
 					indexation = indexation[0]
+
+					# TODO Corriger les erreurs d'encodage dans l'indexation
+					if "&" in indexation or "<" in indexation or indexation == None:
+						#print(note)
+						pass
+
 					if indexation not in dict_indexations:
 						dict_indexations[indexation] = []
 					dict_indexations[indexation].append(uuid)
@@ -95,12 +110,30 @@ for opentheso_personne_uri, p, o in input_graph.triples((None, RDF.type, SKOS.Co
 
 	# TODO Ajouter les altLabels
 
-	#Insertion des concepts et de leurs infos dans Directus
-	r = requests.post(secret["url"] + '/items/personnes?access_token=' + access_token, json=dict_infos_concept)
+	# ENVOI DES DONNEES
+	# r = requests.post(secret["url"] + '/items/personnes?access_token=' + access_token, json=dict_infos_concept)
 
+	# RECUPERATION DES ALTLABELS
+	altlabels = list(input_graph.objects(opentheso_personne_uri, SKOS.altLabel))
+	if altlabels != None:
+		for altlabel in altlabels:
+			dict_altlabels_concept = {
+				"id": uuid,
+				"personnes_altlabels": [{
+					"label": altlabel.value,
+					"personne": uuid
+				}]
+			}
 
-# Fichier json de l'indexation des sources
-json_indexation_source = {}
+			# pprint(dict_altlabels_concept)
+			# ENVOI DES DONNEES
+			r = requests.patch(secret["url"] + '/items/personnes?access_token=' + access_token, json=dict_altlabels_concept)
+	else:
+		pass
+
+#########################################################################################
+## INSERTION DES INDEXATION DES SOURCES DANS DIRECTUS
+#########################################################################################
 
 for k, v in dict_indexations.items():
 	dict_infos_index = {
@@ -114,7 +147,7 @@ for k, v in dict_indexations.items():
 
 	# pprint(dict_infos_index)
 
-	# Insertion des indexations d'articles dans Directus
-	# r = requests.post(secret["url"] + '/items/sources_articles?access_token=' + access_token, json=json_indexation_source)
+	# ENVOI DES DONNEES
+	# r = requests.post(secret["url"] + '/items/sources_articles?access_token=' + access_token, json=dict_infos_index)
 
 
