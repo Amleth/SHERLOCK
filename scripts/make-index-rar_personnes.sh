a044529adf66b7fdba7d399387abeacab6bcc4ef
Rebecca Bristow <58423312+rbbristow@users.noreply.github.com>
2021-07-07 15:47:43 +0200

    create index personnes

---
+++ scripts/make-index-rar_personnes.sh
@@ -0,0 +1,3 @@
+python3 ./indices/rar_personnes.py --dburi http://localhost:3030/mercure_galant/sparql --json ./out/indices/rar_personnes.json
+# Rebecca : http://localhost:3030/mercure_galant/sparql
+# Thomas : http://localhost:3030/iremus/sparql
